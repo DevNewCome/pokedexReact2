b173122d397eb91f13f86e4ece099b7a9f5a9793
Jefferson <jefferson.senac1@yahoo.com>
2023-10-17 22:29:42 -0300

    adicionando novas features

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -2,11 +2,14 @@ import './index.css';
 import { useState, useEffect } from 'react';
 import Api from '../../api/index';
 import { useNavigate } from 'react-router-dom';
-import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom';
+import axios from 'axios';
 
 export default function Home() {
     const [pokemonData, setPokemonData] = useState([]);
-    const navigate = useNavigate
+    const [nomePokemon, setNomePokemon] = useState('');
+    const [pokemonPesquisado, setPokemonPesquisado] = useState(null);
+    const navigate = useNavigate();
 
     useEffect(() => {
         const fetchData = async () => {
@@ -27,38 +30,94 @@ export default function Home() {
         fetchData();
     }, []);
 
-    console.log(pokemonData);
+    const enterClique = (e) => {
+        if (e.key === 'Enter') {
+            buscaPokemon();
+        }
+    }
+
+    async function buscaPokemon() {
+        try {
+            let api = `https://pokeapi.co/api/v2/pokemon/`;
+            let pokemonResponse = await axios.get(`${api}${nomePokemon}`);
+            setPokemonPesquisado(pokemonResponse.data);
+        } catch {
+            console.log('Erro');
+        }
+    }
+
+    // Filtra os Pokémon com base no nome digitado
+    // retorne o nome dos pokemons que incluem o nome que estou digitandoo
+    const pokemonFiltrado = pokemonData.filter((pokemon) => {
+        return pokemon.name.toLowerCase().includes(nomePokemon.toLowerCase())
+    })
 
     return (
-        <div className='container'>
-            {pokemonData.map((item) => (
-                <div className='pokemon-container' key={item.id}>
-                    <div className='descriptionPokemon'>
-                        <div className='nomePokemon'>
-                            <h2>NAME:</h2>
-                            <span>{item.name}</span>
-                        </div>
-                        <div className='classePokemon'>
-                             <h2>CLASS:</h2>
-                             <ul>
-                                {item.types.map((type, index) => (
-                                    <li key={index}>{type.type.name}</li>
+        <>
+            <div className='inputs'>
+                <input type="text" onKeyDown={enterClique} placeholder='digite o nome' value={nomePokemon} onChange={(e) => setNomePokemon(e.target.value)} />
+            </div>
+            <div className='container'>
+                {nomePokemon ? (
+                      pokemonFiltrado.map((item) => (
+                        <div className='pokemon-container' key={item.id}>
+                            <div className='descriptionPokemon'>
+                                <div className='nomePokemon'>
+                                    <h2>NAME:</h2>
+                                    <span>{item.name}</span>
+                                </div>
+                                <div className='classePokemon'>
+                                    <h2>CLASS:</h2>
+                                    <ul>
+                                        {item.types.map((type, index) => (
+                                            <li key={index}>{type.type.name}</li>
+                                        ))}
+                                    </ul>
+                                </div>
+                            </div>
+                            <div className='image-pokemon'>
+                                <img src={item.sprites.front_default} alt={item.name} />
+                            </div>
+                            <div className='habilidades'>
+                                <h2>Powers</h2>
+                                {item.abilities.map((ability) => (
+                                    <span key={ability.ability.name}>{ability.ability.name}<br></br></span>
                                 ))}
-                             </ul>      
+                            </div>
+                            <Link to={`/sobre/${item.id}`}>Ver Sobre</Link>
                         </div>
-                    </div>
-                    <div className='image-pokemon'>
-                        <img src={item.sprites.front_default} alt={item.name} />
-                    </div>
-                    <div className='habilidades'>
-                    <h2>Powers</h2>
-                        {item.abilities.map((ability) => (     
-                            <span key={ability.ability.name}>{ability.ability.name}<br></br></span>
-                        ))}
-                    </div>
-                    <Link to={`/sobre/${item.id}`}>Ver Sobre</Link>
-                </div>
-            ))}
-        </div>
+                    ))
+                ): (
+                    pokemonData.map((item) => (
+                          <div className='pokemon-container' key={item.id}>
+                            <div className='descriptionPokemon'>
+                            <div className='nomePokemon'>
+                                    <h2>NAME:</h2>
+                                    <span>{item.name}</span>
+                                </div>
+                                <div className='classePokemon'>
+                                    <h2>CLASS:</h2>
+                                    <ul>
+                                        {item.types.map((type, index) => (
+                                            <li key={index}>{type.type.name}</li>
+                                        ))}
+                                    </ul>
+                                </div>
+                            </div>
+                                    <div className='image-pokemon'>
+                                <img src={item.sprites.front_default} alt={item.name} />
+                                  </div>
+                                  <div className='habilidades'>
+                                <h2>Powers</h2>
+                                {item.abilities.map((ability) => (
+                                    <span key={ability.ability.name}>{ability.ability.name}<br></br></span>
+                                ))}
+                                 </div>
+                                 <Link to={`/sobre/${item.id}`}>Ver Sobre</Link>
+                            </div>
+                    ))
+                )}
+            </div>
+        </>
     );
 }
